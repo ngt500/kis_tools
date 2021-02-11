0d7fa902b206bedb4ba0f1ae2f4717203d6b79c8
ngt500 <16139108+ngt500@users.noreply.github.com>
2021-02-11 18:25:41 +0000

    fix api url

--- kis_track.py
+++ kis_track.py
@@ -6,10 +6,10 @@ import sys
 def buildAPIUrl(KISMET_HTTP_PROTOCOL, KISMET_HOST, KISMET_PORT, MAC_ADDR, DEBUG, VERBOSE):
     if validateMac(MAC_ADDR):
         if DEBUG:
-            print("%s://%s:%s/devices/by-mac/%s/device.json" % (KISMET_HTTP_PROTOCOL, KISMET_HOST, KISMET_PORT, MAC_ADDR))
+            print("%s://%s:%s/devices/by-mac/%s/devices.json" % (KISMET_HTTP_PROTOCOL, KISMET_HOST, KISMET_PORT, MAC_ADDR))
         if VERBOSE:
             print("- MAC address validated")
-        api_url = ("%s://%s:%s/devices/by-mac/%s/device.json" % (KISMET_HTTP_PROTOCOL, KISMET_HOST, KISMET_PORT, MAC_ADDR))
+        api_url = ("%s://%s:%s/devices/by-mac/%s/devices.json" % (KISMET_HTTP_PROTOCOL, KISMET_HOST, KISMET_PORT, MAC_ADDR))
     else:
         print("Invalid MAC. Make sure you are using : as separators")
         sys.exit()
